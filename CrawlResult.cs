ed9e775993dad58ae2e5a8ac34f86f204bb4d077
Andrey B <justandreyb@gmail.com>
2016-11-07 17:07:19 +0300

    Add files via upload

---
+++ CrawlResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEBCrawler.Model
+{
+    class CrawlResult
+    {
+        Site site;
+        LinkedList<CrawlResult> results;
+
+        public CrawlResult()
+        {
+            results = new LinkedList<CrawlResult>(); 
+        }
+
+        public void setSite(Site site)
+        {
+            this.site = site;
+        }
+
+        public void addResult(CrawlResult result)
+        {
+            this.results.AddLast(result);
+        }
+
+        public Site getSite()
+        {
+            return this.site;
+        }
+
+    }
+}
---
+++ WEBCrawler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEBCrawler.Model
+{
+    class WEBCrawler : IWEBCrawler
+    {
+        int maximalDepth;
+        CrawlResult result;
+
+        public WEBCrawler(int maximalDepth)
+        {
+            this.maximalDepth = maximalDepth;
+            result = new CrawlResult();
+        }
+
+        public async Task<CrawlResult> PerformCrawlingAsync(LinkedList<string> links, int depth)
+        {
+            CrawlResult currentCrawl = new CrawlResult();
+            foreach (string link in links)
+            {
+                try
+                {
+                    Site currentSite = null;
+                    if (depth < maximalDepth)
+                    {
+                        currentSite = SiteSurfer.watchSite(link);
+                        if (currentSite.InnerLinks != null)
+                        {
+                            currentCrawl = await PerformCrawlingAsync(currentSite.InnerLinks, depth + 1);
+                        }
+                        if (currentCrawl != null)
+                        {
+                            result.addResult(currentCrawl);
+                        }
+                        currentCrawl.setSite(currentSite);
+                    }
+
+                }
+                catch {
+                    throw new Exception();
+                }
+            }
+            return result;
+        }
+    }
+}
